19850da0b67b60d82a86bad8af4d81501563393c
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-07-05 00:56:45 -0700

    Implement solutions to promises exercises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,30 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var githubProfileAsync = require('./promisification').getGitHubProfileAsync;
+var writeFileAsync = Promise.promisify(fs.writeFile);
+var readFileAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+  // return readFileAsync(readFilePath)
+  return new Promise((resolve, reject) => {
+    fs.readFile(readFilePath, 'utf8', (err, file) => {
+      if (err) {
+        throw(err);
+        // reject(err);
+      } else {
+        var username = file.split('\n')[0];
+        return resolve(username);
+      }
+    });
+  })
+
+  .then((username) => { return githubProfileAsync(username) })
   
+  .then((profile) => { return writeFileAsync(writeFilePath, JSON.stringify(profile)) })
+
+  .catch()
+  // .error()
 };
 
 // Export these functions so we can test them
